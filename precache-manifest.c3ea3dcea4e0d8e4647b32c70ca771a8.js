acb85723f63f1272edbd5f7cb948d23a4dd00623
S3-D1 <39882282+S3-D1@users.noreply.github.com>
2020-05-02 11:37:38 +0000

    Update gh-pages to output generated at 354b26a

--- precache-manifest.c3ea3dcea4e0d8e4647b32c70ca771a8.js
+++ precache-manifest.c3ea3dcea4e0d8e4647b32c70ca771a8.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c56dda1f778095d0b82c",
-    "url": "/css/chunk-vendors.56821072.css"
+    "revision": "f97aa9fba817ac1bea22",
+    "url": "/css/chunk-vendors.1f5488f8.css"
   },
   {
     "revision": "1d95f11bc64889457c9f85f0701da31c",
     "url": "/datenschutzerklaerung.html"
   },
   {
-    "revision": "2ba27eff4d1bf2a2de38bf81416b0ea4",
+    "revision": "ef1d663b09d65e2ba9234789d4bc86b5",
     "url": "/index.html"
   },
   {
-    "revision": "f60a6f4f100b88a56583",
-    "url": "/js/about.377477c7.js"
+    "revision": "276fa51ead9799ba5ca5",
+    "url": "/js/about.23783b7e.js"
   },
   {
-    "revision": "b8d50d57ece8963e0929",
-    "url": "/js/app.ba34390c.js"
+    "revision": "6d9da36f134d9e7b4d8f",
+    "url": "/js/app.cd0012ca.js"
   },
   {
-    "revision": "c56dda1f778095d0b82c",
-    "url": "/js/chunk-vendors.81592d18.js"
+    "revision": "f97aa9fba817ac1bea22",
+    "url": "/js/chunk-vendors.52c365fe.js"
   },
   {
     "revision": "ad234c8535fae04e20b8c55b8c81b853",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a92ca569e7af31146411fe14684f89f9.js"
+  "/precache-manifest.c3ea3dcea4e0d8e4647b32c70ca771a8.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "untitled"});
